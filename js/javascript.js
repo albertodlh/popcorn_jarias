a5acb4e7a1c41fef0267a506999f083a3bf57c3b
Alberto De la Hoz <alberto.delahoz@koombea.com>
2016-01-12 16:48:59 -0500

    Añade template de handlebars

--- js/javascript.js
+++ js/javascript.js
@@ -1,29 +1,12 @@
 var apiBaseUrl = 'https://api.themoviedb.org/3/movie/';
 var apiKeyString = '?api_key=300d2fb47e3f5f8d5e569ce27884acdc';
-
-function listado(){
-  $.get(apiBaseUrl + "top_rated" + apiKeyString, function( data ) {
-    console.log( data );
-    for (var i = 0; i < data.results.length; i++) {
-      $( "#lista" ).append( "<li style='list-style: none;'>" + data.results[i].original_title + "</li>" );
-    };
-  });
-  // peliculas('popular', 0);
-}
+var movieDetailsTemplate = Handlebars.compile($("#movie-template").html());
 
 function information() {
   var id = $(this).data('movieId');
   $.get(apiBaseUrl + id + apiKeyString, function(data){
-    console.log(data);
-    $("#container-information").html("");
-    $("#container-information").append("<h1 class='sub-title'>Information</h1>");
-    $("#container-information").append("<div id='cont-image-infor'></div>");
-    //$("#container-information").append("<div id='cont-data-infor'></div>");
-    $("#container-information").append("<div id='cont-description-infor'></div>");
-    $("#cont-image-infor").append("<img  class='backdrop_path' src='http://image.tmdb.org/t/p/w300/" + data.backdrop_path + "' />");
-    $("#cont-image-infor").append("<div class='cont-data-infor'><p><span class='title-data'>Title: </span>" + data.original_title + "</p><p><span class='title-data'>Tagline: </span>" + data.tagline + "</p><p><span class='title-data'>Revenue: </span>" + data.revenue + "</p><div/>");
-    $("#cont-description-infor").append("<p>" + data.overview + "<p/>");
-    //$("#cont-data-infor").append("<p>" + data.overview + "<p/>");
+    var compiledHtml = movieDetailsTemplate(data);
+    $("#container-information").html(compiledHtml);
   });
 }
 
@@ -36,8 +19,8 @@ function peliculas(tipo){
       $imgEl = $('<img/>')
         .addClass('images-peliculas')
         .attr('src', imgSrc)
-        .data('movieId', data.results[i].id);
-      $imgEl.on('click', information);
+        .data('movieId', data.results[i].id)
+        .on('click', information);
       $("#" + tipo).append($imgEl);
     };
   });
