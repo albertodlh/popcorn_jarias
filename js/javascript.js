7a0a5f60b813f41e5c468632b9fcaefd0834b55c
Alberto De la Hoz <alberto.delahoz@koombea.com>
2016-01-12 11:23:26 -0500

    Carga las películas dependiendo del id del contenedor

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,26 @@
-function traer(){
+function listado(){
   $.get( "https://api.themoviedb.org/3/movie/top_rated?api_key=300d2fb47e3f5f8d5e569ce27884acdc", function( data ) {
     console.log( data );
     for (var i = 0; i < data.results.length; i++) {
       $( "#lista" ).append( "<li style='list-style: none;'>" + data.results[i].original_title + "</li>" );
     };
   });
-  peliculas('popular', 0);
+  // peliculas('popular', 0);
 }
 
-function peliculas(pelicula, sw){
+function peliculas(pelicula){
   $.get( "https://api.themoviedb.org/3/movie/" + pelicula + "?api_key=300d2fb47e3f5f8d5e569ce27884acdc", function( data ) {
     console.log( data );
     for (var i = 0; i < data.results.length; i++) {
       $("#" + pelicula).append("<img class='images-peliculas' src='http://image.tmdb.org/t/p/w300/" + data.results[i].poster_path + "'/>");
     };
   });
-  if(sw == 0) peliculas('top_rated', 1);
-  else if(sw == 1) peliculas('upcoming', 2);
+  //if(sw == 0) peliculas('top_rated', 1);
+  //else if(sw == 1) peliculas('upcoming', 2);
 }
+
+$(function(){
+  $('.js-movie-container').each(function(inx){
+    peliculas(this.id);
+  });
+});
